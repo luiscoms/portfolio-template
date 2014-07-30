952da6f2824ad754b053b50722b5e35915b10def
LuisComS <dev@luiscoms.com.br>
2014-07-30 18:04:40 -0300

    Added clean tmp dir to build

--- bin/buid-deploy-site.sh
+++ bin/buid-deploy-site.sh
@@ -7,6 +7,8 @@ DIRTO="~/www/";
 cd $BIN_DIR/../site/
 grunt
 
+ssh luiscoms@luiscoms.com.br "rm -r "$DIRFROM
+
 scp -r build luiscoms.com.br:~/tmp/
 
 ssh luiscoms@luiscoms.com.br "rsync -ruv "$DIRFROM" "$DIRTO
