9efe51bd1ef43096b171c971795f595d534c8ac3
LuisComS <dev@luiscoms.com.br>
2014-07-30 16:46:28 -0300

    Added proccess-html and changed GPlus link

--- site/GruntFile.js
+++ site/GruntFile.js
@@ -6,39 +6,103 @@ module.exports = function(grunt) {
       dist: ["dist"]
     },
     concat: {
-      dist: {
-        src: [ 'js/jquery.min.js', 'js/jquery.easing.1.3.js', './js/jquery.isotope.min.js', 'js/jquery.elastislide.js', 'js/jquery.prettyPhoto.js', 'js/klass.min.js', 'js/code.photoswipe.jquery-3.0.4.min.js', 'js/jquery.jtwt.min.js', 'js/jquery.sidr.min.js', 'js/responsiveslides.min.js', 'js/scripts.js' ],
+      options: {
+        stripBanners: {
+          block: true
+        },
+      },
+      js: {
+        separator: ';',
+        src: [
+          'js/jquery.min.js',
+          'js/jquery.easing.1.3.js',
+          // 'js/jquery.isotope.min.js',
+          // 'js/jquery.elastislide.js',
+          // 'js/jquery.prettyPhoto.js',
+          // 'js/klass.min.js',
+          // 'js/code.photoswipe.jquery-3.0.4.min.js', // requires klass
+          // 'js/jquery.jtwt.min.js', // twitter
+          'js/jquery.sidr.min.js', // menu bar
+          // 'js/responsiveslides.min.js',
+          'js/scripts.js'
+        ],
         dest: 'build/js/scripts.js'
+      },
+      css: {
+        separator: ';',
+        src: [
+          'css/o-responsive.css',
+          'css/style.css',
+          'css/sugar.css',
+          // 'css/prettyPhoto.css',
+          // 'css/photoswipe.css',
+          'css/glyphicons.css'
+        ],
+        dest: 'build/css/style.css'
       }
     },
-    sass: {
-      dist: {
-        options: {
-          style: 'expanded'
-        },
-        // files: {
-        //   'build/css/style.css': 'css/style.css',
-        // }
+    copy: {
+      build: {
         files: [{
-          expand: true,
-          // cwd: 'styles',
-          src: ['css/*.css'],
-          dest: 'build',
-          ext: '.css'
+          src: 'fonts/**/*',
+          dest: 'build/'
+        },{
+          src: 'images/**/*',
+          dest: 'build/'
         }]
       }
     },
     notify: {
-      buid: {
+      build: {
         options: {
           message: 'Buid completed'
         }
       }
+    },
+    processhtml: {
+      build: {
+        options: {
+          process: true,
+          data: {
+            title: 'My app',
+            message: 'This is production distribution'
+          }
+        },
+        files: {
+          'build/index.html': ['index.html']
+        }
+      }
+    },
+    sass: {
+      build: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'build/css/o-responsive.css': 'css/o-responsive.css',
+          'build/css/style.css': 'css/style.css',
+          'build/css/sugar.css': 'css/sugar.css',
+          // 'build/css/prettyPhoto.css': 'css/prettyPhoto.css',
+          // 'build/css/photoswipe.css': 'css/photoswipe.css',
+          'build/css/glyphicons.css': 'css/glyphicons.css'
+          // 'build/css/style.css': 'css/style.scss'
+        }
+        // files: [{
+        //   expand: true,
+        //   // cwd: 'styles',
+        //   src: ['css/*.css'],
+        //   dest: 'build',
+        //   ext: '.css'
+        // }]
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-notify');
-  grunt.registerTask('default', ['clean', 'sass', 'concat', 'notify:buid']);
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.registerTask('build', ['clean', 'concat', 'processhtml:build', 'copy:build', 'notify:build']);
+  grunt.registerTask('default', ['build']);
 };
