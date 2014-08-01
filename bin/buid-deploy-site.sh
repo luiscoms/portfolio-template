f5735a10f1e3888a490e8d2dfdc223c954faa7e3
LuisComS <dev@luiscoms.com.br>
2014-08-01 03:11:50 -0300

    Changed deploy script to scoop

--- bin/buid-deploy-site.sh
+++ bin/buid-deploy-site.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-BIN_DIR=$(readlink -f $(dirname $0));
+#BIN_DIR=$(readlink -f $(dirname $0));
+BIN_DIR=$(pwd);
 DIRFROM="~/tmp/";
 DIRTO="~/www/";
 
@@ -9,6 +10,6 @@ grunt
 
 ssh luiscoms@luiscoms.com.br "rm -r "$DIRFROM
 
-scp -r build luiscoms.com.br:~/tmp/
+scp -r build luiscoms@luiscoms.com.br:~/tmp/
 
 ssh luiscoms@luiscoms.com.br "rsync -ruv "$DIRFROM" "$DIRTO
