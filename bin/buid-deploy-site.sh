da69ca4ac53ff2c1325eabc1703e86355344eb6a
LuisComS <dev@luiscoms.com.br>
2014-08-28 19:59:38 -0300

    Fixed deploy script

--- bin/buid-deploy-site.sh
+++ bin/buid-deploy-site.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
+ZIPNAME="porfolio"
 #BIN_DIR=$(readlink -f $(dirname $0));
-BIN_DIR=$(pwd);
+BIN_DIR=$(pwd)/$(dirname $0);
 DIRFROM="~/tmp/";
 DIRTO="~/www/";
 
+echo "BIN_DIR: "$BIN_DIR
 cd $BIN_DIR/../site/
 grunt
 
-ssh luiscoms@luiscoms.com.br "rm -r "$DIRFROM
+cd build
 
-scp -r build luiscoms@luiscoms.com.br:~/tmp/
+# zip filename -r files
+zip ../$ZIPNAME -r .
 
-ssh luiscoms@luiscoms.com.br "rsync -ruv "$DIRFROM" "$DIRTO
+cd ..
+
+ssh luiscoms@luiscoms.com.br "rm -rf "$DIRFROM"; mkdir -p "$DIRFROM
+
+# scp -r build luiscoms@luiscoms.com.br:$DIRFROM
+scp $ZIPNAME.zip luiscoms@luiscoms.com.br:$DIRFROM
+
+ssh luiscoms@luiscoms.com.br "unzip -o "$DIRFROM/$ZIPNAME.zip" -d "$DIRTO
+# ssh luiscoms@luiscoms.com.br "rsync -ruv "$DIRFROM" "$DIRTO
+
+rm $ZIPNAME.zip
